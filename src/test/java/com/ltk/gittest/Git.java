0281b02da3e7675564bdfbf09035e178f458791b
Lunuyh <2199956452@qq.com>
2021-11-22 12:46:39 +0800

    second commit

--- src/test/java/com/ltk/gittest/Git.java
+++ src/test/java/com/ltk/gittest/Git.java
@@ -3,5 +3,6 @@ package com.ltk.gittest;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello,上善若水");
+        System.out.println("hello,温柔乡");
     }
 }
