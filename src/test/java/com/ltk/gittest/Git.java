ad05cf173b6f05c4bcab004273a55b8b8e57a343
Lunuyh <84300109+Lunuyh@users.noreply.github.com>
2021-11-22 13:29:41 +0800

    Update Git.java

--- src/test/java/com/ltk/gittest/Git.java
+++ src/test/java/com/ltk/gittest/Git.java
@@ -7,5 +7,6 @@ public class Git {
         System.out.println("hello,成电");
         System.out.println("谢谢你，内心深处的善良");
         System.out.println("暖又平静");
+        System.out.println("自强不息");
     }
 }
