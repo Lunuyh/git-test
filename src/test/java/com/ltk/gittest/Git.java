4df005085b4dec20b1e2dab06677dd8cce4560de
Lunuyh <2199956452@qq.com>
2021-11-22 12:47:29 +0800

    third commit

--- src/test/java/com/ltk/gittest/Git.java
+++ src/test/java/com/ltk/gittest/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello,上善若水");
         System.out.println("hello,温柔乡");
+        System.out.println("hello,成电");
     }
 }
